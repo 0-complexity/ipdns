cb7f85c7df0722cf9db1b92a77c7ab8b769ceccb
Geert Audenaert <geert@mobicage.com>
2017-01-31 15:29:14 +0100

    Fixed download_url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Geert Audenaert',
   author_email = 'geert@greenitglobe.com',
   url = 'https://github.com/0-complexity/ipdns',
-  download_url = 'https://github.com/0-complexity/ipdns/tarball/0.1',
+  download_url = 'https://github.com/0-complexity/ipdns/tarball/0.1.4',
   keywords = ['ip', 'dns', 'twisted'],
   classifiers = [],
   install_requires=['twisted', 'netaddr'],
